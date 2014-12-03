3fd6587f1a5a2e5930967af61dd714c88e44143e
hmwverbeek <h.m.w.verbeek@tue.nl>
2014-12-03 09:35:23 +0000

    Added Berthelot reduction of implicit places.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/Murata/Trunk@17578 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/parameters/BerthelotParameters.java
@@ -0,0 +1,53 @@
+package org.processmining.parameters;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.processmining.models.semantics.petrinet.Marking;
+
+public class BerthelotParameters {
+
+	private Marking initialMarking;
+	private Set<Marking> finalMarkings;
+	private Marking initialBerthelotMarking;
+	private Set<Marking> finalBerthelotMarkings;
+	
+	public BerthelotParameters() {
+		setInitialMarking(new Marking());
+		setFinalMarkings(new HashSet<Marking>());
+		setInitialBerthelotMarking(null);
+		setFinalBerthelotMarkings(null);
+	}
+	
+	public Marking getInitialMarking() {
+		return initialMarking;
+	}
+
+	public void setInitialMarking(Marking initialMarking) {
+		this.initialMarking = initialMarking;
+	}
+
+	public Set<Marking> getFinalMarkings() {
+		return finalMarkings;
+	}
+
+	public void setFinalMarkings(Set<Marking> finalMarkings) {
+		this.finalMarkings = finalMarkings;
+	}
+
+	public Marking getInitialBerthelotMarking() {
+		return initialBerthelotMarking;
+	}
+
+	public void setInitialBerthelotMarking(Marking initialBerthelotMarking) {
+		this.initialBerthelotMarking = initialBerthelotMarking;
+	}
+
+	public Set<Marking> getFinalBerthelotMarkings() {
+		return finalBerthelotMarkings;
+	}
+
+	public void setFinalBerthelotMarkings(Set<Marking> finalBerthelotMarkings) {
+		this.finalBerthelotMarkings = finalBerthelotMarkings;
+	}
+}
